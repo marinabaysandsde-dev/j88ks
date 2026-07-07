80ace1a1f5060a86c5773ebe0e0f9ddb2558901d
j88ks <j88ks@expo.dev>
2026-07-07 06:49:28 +0000

    fix: convert mipmap webp/png mismatches, add favicon assets, rename @ files

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: "https://media.shb32.com/public/j88ks/site-ttkm/09fdcfbe-277d-49f8-8918-d8b1ff8dc3e9.png" }}
+        source={{ uri: "/loading.png" }}
         style={styles.image}
         resizeMode="contain"
       />
